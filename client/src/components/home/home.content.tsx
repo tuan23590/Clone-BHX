b404c4fcd612bfe3abe17a5ce36699a64e82f4bd
tuan23590 <thanhtuan.htcv@gmail.com>
2024-11-15 23:17:23 +0700

    Refactor HomeContent component to use useEffect for modifying category list and improve rendering logic

--- client/src/components/home/home.content.tsx
+++ client/src/components/home/home.content.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Box, Link, Typography } from "@mui/joy";
-import React from "react";
+import React, { useEffect } from "react";
 import ListCategories from "./listCategories";
 import CategoryPage from "./category/category";
 
@@ -9,73 +9,92 @@ type ListCategoriesProps = {
 };
 
 export default function HomeContent({ listCategories }: ListCategoriesProps) {
-  const modifiedListCategories = listCategories.map((category) => {
-    const products = category.products.slice(0, 5);
-    products.forEach((product : any) => {
-      product.variations = product.variations.slice(0, 1);
-    });
-    return { ...category, products };
-  });
+  const [modifiedListCategories, setModifiedListCategories] = React.useState<
+    any[]
+  >([]);
+  useEffect(() => {
+    setModifiedListCategories(
+      listCategories.map((category) => {
+        const products = category.products.slice(0, 5);
+        products.forEach((product: any) => {
+          product.variations = product.variations.slice(0, 1);
+        });
+        return { ...category, products };
+      })
+    );
+  }, []);
   return (
     <Box>
       <ListCategories listCategories={listCategories} />
-     <Box sx={{
-      height: "75vh",
-      overflowX: "auto",
-      scrollbarWidth: "thin",
-     }}>
-     {modifiedListCategories.map(
-        (category) =>
-          category.products.length > 0 && (
-            <Box
-              key={category._id}
-              sx={{
-                backgroundColor: "#e8f5e9",
-                borderTop: "1px solid #00AC5B",
-                marginBottom: 2,
-                paddingLeft: 2,
-              }}
-            >
+      <Box
+        sx={{
+          height: "75vh",
+          overflowX: "auto",
+          scrollbarWidth: "thin",
+        }}
+      >
+        {modifiedListCategories.map(
+          (category) =>
+            category.products.length > 0 && (
               <Box
+                key={category._id}
                 sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  marginBottom: 1,
+                  backgroundColor: "#e8f5e9",
+                  borderTop: "1px solid #00AC5B",
+                  marginBottom: 2,
+                  paddingLeft: 2,
                 }}
               >
-                <Typography
-                  level="h4"
+                <Box
                   sx={{
-                    paddingX: 2,
-                    backgroundColor: "#00AC5B",
-                    color: "#fff",
-                    borderRadius: "0 0 5px 5px",
+                    display: "flex",
+                    justifyContent: "center",
+                    marginBottom: 1,
                   }}
                 >
-                  {category.name}
-                </Typography>
-              </Box>
-                 
-              <CategoryPage data={category} />
-              <Box sx={{
-                display: "flex",
-                justifyContent: "center",
-                marginBottom: 2,
-              }}>
-                <Link href={`/${category._id}`} underline="none" color="neutral" sx={{
-                  paddingY: 1,
-                }}>
-                  Xem thêm<span style={{
-                    fontWeight: "bold",
-                    marginLeft: 5,
+                  <Typography
+                    level="h4"
+                    sx={{
+                      paddingX: 2,
+                      backgroundColor: "#00AC5B",
+                      color: "#fff",
+                      borderRadius: "0 0 5px 5px",
+                    }}
+                  >
+                    {category.name}
+                  </Typography>
+                </Box>
+
+                <CategoryPage data={category} />
+                <Box
+                  sx={{
                     display: "flex",
-                  }}>{`${category.products.length} ${category.name} >`}</span>
-                </Link>
+                    justifyContent: "center",
+                    marginBottom: 2,
+                  }}
+                >
+                  <Link
+                    href={`/${category._id}`}
+                    underline="none"
+                    color="neutral"
+                    sx={{
+                      paddingY: 1,
+                    }}
+                  >
+                    Xem thêm
+                    <span
+                      style={{
+                        fontWeight: "bold",
+                        marginLeft: 5,
+                        display: "flex",
+                      }}
+                    >{`${category.products.length} ${category.name} >`}</span>
+                  </Link>
+                </Box>
               </Box>
-            </Box>
-          )
-      )}
-     </Box>
+            )
+        )}
+      </Box>
     </Box>
   );
 }
